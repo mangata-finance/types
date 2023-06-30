9bd8776e391e508cba114d4c02d82dc0e1126d5c
devdanco <devdanco@gmail.com>
2023-06-30 14:43:16 +0200

    feat: add lookup types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { OverrideBundleDefinition } from "@polkadot/types/types";
 import { ApiOptions } from "@polkadot/api/types";
+import type { RegistryTypes } from "@polkadot/types/types";
 
-import * as definitions from "./interfaces/definitions";
+import { mTypes, mRpc } from "@mangata-finance/type-definitions";
+
+import "@polkadot/types/lookup";
 
 import "./interfaces/augment-api";
 import "./interfaces/augment-api-consts";
@@ -13,223 +15,7 @@ import "./interfaces/augment-api-tx";
 import "./interfaces/augment-types";
 import "./interfaces/augment-api-runtime";
 
-export const mTypes = Object.values(definitions).reduce(
-  (res: Record<string, any>, { types }): Record<string, any> => ({
-    ...res,
-    ...types
-  }),
-  {}
-);
-
-export const mRpc = {
-  xyk: {
-    calculate_buy_price: {
-      description: "",
-      params: [
-        {
-          name: "input_reserve",
-          type: "Balance"
-        },
-        {
-          name: "output_reserve",
-          type: "Balance"
-        },
-        {
-          name: "sell_amount",
-          type: "Balance"
-        }
-      ],
-      type: "XYKRpcResult<Balance>"
-    },
-    calculate_sell_price: {
-      description: "",
-      params: [
-        {
-          name: "input_reserve",
-          type: "Balance"
-        },
-        {
-          name: "output_reserve",
-          type: "Balance"
-        },
-        {
-          name: "sell_amount",
-          type: "Balance"
-        }
-      ],
-      type: "XYKRpcResult<Balance>"
-    },
-    get_burn_amount: {
-      description: "",
-      params: [
-        {
-          name: "first_asset_id",
-          type: "TokenId"
-        },
-        {
-          name: "second_asset_id",
-          type: "TokenId"
-        },
-        {
-          name: "liquidity_asset_amount",
-          type: "Balance"
-        }
-      ],
-      type: "RPCAmountsResult<Balance>"
-    },
-    calculate_sell_price_id: {
-      description: "",
-      params: [
-        {
-          name: "sold_token_id",
-          type: "TokenId"
-        },
-        {
-          name: "bought_token_id",
-          type: "TokenId"
-        },
-        {
-          name: "sell_amount",
-          type: "Balance"
-        }
-      ],
-      type: "XYKRpcResult<Balance>"
-    },
-    calculate_buy_price_id: {
-      description: "",
-      params: [
-        {
-          name: "sold_token_id",
-          type: "TokenId"
-        },
-        {
-          name: "bought_token_id",
-          type: "TokenId"
-        },
-        {
-          name: "buy_amount",
-          type: "Balance"
-        }
-      ],
-      type: "XYKRpcResult<Balance>"
-    },
-    calculate_rewards_amount: {
-      description: "",
-      params: [
-        {
-          name: "user",
-          type: "AccountId"
-        },
-        {
-          name: "liquidity_asset_id",
-          type: "TokenId"
-        }
-      ],
-      type: "XYKRpcResult<Balance>"
-    },
-    calculate_balanced_sell_amount: {
-      description: "",
-      params: [
-        {
-          name: "total_amount",
-          type: "Balance"
-        },
-        {
-          name: "reserve_amount",
-          type: "Balance"
-        }
-      ],
-      type: "XYKRpcResult<Balance>"
-    },
-    get_max_instant_unreserve_amount: {
-      description: "",
-      params: [
-        {
-          name: "user",
-          type: "AccountId"
-        },
-        {
-          name: "liquidity_asset_id",
-          type: "TokenId"
-        }
-      ],
-      type: "Balance"
-    },
-    get_max_instant_burn_amount: {
-      description: "",
-      params: [
-        {
-          name: "user",
-          type: "AccountId"
-        },
-        {
-          name: "liquidity_asset_id",
-          type: "TokenId"
-        }
-      ],
-      type: "Balance"
-    },
-    is_sell_asset_lock_free: {
-      description: "",
-      params: [
-        {
-          name: "path",
-          type: "Vec<TokenId>"
-        },
-        {
-          name: "input_amount",
-          type: "Balance"
-        }
-      ],
-      type: "bool"
-    },
-    is_buy_asset_lock_free: {
-      description: "",
-      params: [
-        {
-          name: "path",
-          type: "Vec<TokenId>"
-        },
-        {
-          name: "input_amount",
-          type: "Balance"
-        }
-      ],
-      type: "bool"
-    }
-  },
-  vesting: {
-    getVestingLockedAt: {
-      description: "",
-      params: [
-        {
-          name: "who",
-          type: "AccountId"
-        },
-        {
-          name: "token_id",
-          type: "TokenId"
-        }
-      ],
-      type: "VestingInfosWithLockedAt<Balance, BlockNumber>"
-    }
-  }
-};
-
-export const typesBundleForPolkadotApps: OverrideBundleDefinition = {
-  types: [
-    {
-      minmax: [0, undefined],
-      types: mTypes
-    }
-  ],
-  rpc: mRpc
-};
-
-export const defaultOptions: ApiOptions = {
-  types: mTypes,
-  rpc: mRpc
-};
+import * as lookupTypes from "./interfaces/lookup";
 
 export const options = ({
   types = {},
@@ -238,6 +24,7 @@ export const options = ({
 }: ApiOptions = {}): ApiOptions => ({
   types: {
     ...mTypes,
+    ...(lookupTypes as unknown as RegistryTypes),
     ...types
   },
   rpc: {
